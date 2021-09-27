e8f384410533d760508e52e2aac0f5c3c8863b31
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-09-27 15:16:25 +0200

    added more camera controls

--- table_camera.gd
+++ table_camera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
-var _inverted := false
+export var _inverted := false
+export var _zoom_factor := 0.1
 
 var _being_dragged := false
 
@@ -9,6 +10,12 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_MIDDLE:
 			_being_dragged = event.pressed
+		
+		elif event.button_index == BUTTON_WHEEL_UP:
+			zoom -= Vector2(_zoom_factor, _zoom_factor)
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			zoom += Vector2(_zoom_factor, _zoom_factor)
 	
-	if _being_dragged and event is InputEventMouseMotion:
-		position += event.relative * (1.0 if _inverted else -1.0)
+	elif event is InputEventMouseMotion:
+		if _being_dragged:
+			position += event.relative * (1.0 if _inverted else -1.0) * zoom.length()
