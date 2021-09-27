5172cce3f918a6ac3d0579459c5ebd4bcf771592
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-09-27 14:13:51 +0200

    added hp bar

---
+++ life_bar.gd
@@ -0,0 +1,17 @@
+extends ProgressBar
+
+
+onready var _current: Label = get_node("../HBoxContainer/Current")
+onready var _max: Label = get_node("../HBoxContainer/Max")
+
+
+func _ready() -> void:
+	_on_value_changed(value)
+	_on_changed()
+
+
+func _on_value_changed(new_value: float) -> void:
+	_current.text = "%d" % new_value
+
+func _on_changed() -> void:
+	_max.text = "%d" % max_value
