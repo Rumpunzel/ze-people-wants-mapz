b75da67b2179734be58a43d4ae4b26053d582d10
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-10-09 17:07:39 +0200

    added session mode

--- main.gd
+++ main.gd
@@ -1,14 +1,15 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+export var _run_as_session := true
 
 
 # Called when the node enters the scene tree for the first time.
 func _enter_tree() -> void:
 	randomize()
+	
+	if _run_as_session:
+		OS.current_screen = OS.get_screen_count() - 1
+		OS.window_fullscreen = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
