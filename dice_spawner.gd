3163c950d6ba871da3410353493071cf536cb7af
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-10-10 19:11:25 +0200

    fixed array out of bounds exception

--- dice_spawner.gd
+++ dice_spawner.gd
@@ -3,6 +3,8 @@ extends LineEdit
 
 func _spawn_dice(new_text: String) -> void:
 	var commands := new_text.split("d")
+	if commands.size() <= 1:
+		return
 	
 	for _i in range(int(max(int(commands[0]), 1))):
 		ShittySingleton.left_to_spawn.append(Die.DieToRoll.new(int(commands[1])))
